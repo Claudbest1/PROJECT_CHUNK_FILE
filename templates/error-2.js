537c421fb24a113d352afcb562dc0127adc4d369
gobscat <105522161+gobscat@users.noreply.github.com>
2022-08-11 19:28:56 +0100

    Create error-2.js

---
+++ templates/error-2.js
@@ -0,0 +1,7 @@
+errorMessage2=document.querySelector('.error-2-content');
+errorKey2=document.querySelector('.error-2-key');
+console.log(errorKey2)
+
+errorKey2.addEventListener('click',()=>{
+    errorMessage2.style.display='none'
+})
