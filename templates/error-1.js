157a2b8f766794c5b779c98747873bd6a45c9dc7
gobscat <105522161+gobscat@users.noreply.github.com>
2022-08-11 15:27:57 +0100

    Create error-1.js

---
+++ templates/error-1.js
@@ -0,0 +1,8 @@
+errorMessage1=document.getElementById('error-1-message');
+returnKey=document.querySelector('#return-key');
+
+
+returnKey.addEventListener('click',()=>{
+    errorMessage1.style.display='none'
+})
+
